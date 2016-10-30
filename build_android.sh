908aaa169778645194f2291e9ba038d9939b5f2e
Sherlock <sherlock.towne@gmail.com>
2016-10-31 01:29:43 +0800

    update: updated building script to compile FFmpeg to one shared library.

--- build_android.sh
+++ build_android.sh
@@ -8,8 +8,8 @@ function build_one
 {
 ./configure \
     --prefix=$PREFIX \
-    --enable-shared \
-    --disable-static \
+    --enable-static \
+    --disable-shared \
     --disable-doc \
     --disable-ffmpeg \
     --disable-ffplay \
@@ -22,13 +22,53 @@ function build_one
     --target-os=linux \
     --arch=arm \
     --enable-cross-compile \
+    --enable-runtime-cpudetect \
+    --disable-asm \
+    --disable-stripping \
+    # --cc=$TOOLCHAIN/bin/arm-linux-androideabi-gcc \
+    # --nm=$TOOLCHAIN/bin/arm-linux-androideabi-nm \
+    --enable-version3 \
+    --enable-small \
+    --disable-vda \
+    --disable-iconv \
+    --disable-encoders \
+    --disable-decoders \
+    --enable-decoder=aac \
+    --enable-decoder=aac_latm \
+    --enable-decoder=h264 \
+    --disable-muxers \
+    --disable-demuxers \
+    --enable-demuxer=h264 \
+    --disable-parsers \
+    --enable-parser=aac \
+    --enable-parser=ac3 \
+    --enable-parser=h264 \
+    --disable-protocols \
+    --enable-protocol=file \
+    --enable-protocol=rtmp \
+    --disable-bsfs \
+    --enable-bsf=aac_adtstoasc \
+    --enable-bsf=h264_mp4toannexb \
+    --disable-indevs \
+    --enable-zlib \
+    --disable-outdevs \
+    --disable-debug \
     --sysroot=$SYSROOT \
     --extra-cflags="-Os -fpic $ADDI_CFLAGS" \
     --extra-ldflags="$ADDI_LDFLAGS" \
     $ADDITIONAL_CONFIGURE_FLAG
 make clean
-make
+make -j8
 make install
+
+$TOOLCHAIN/bin/arm-linux-androideabi-ld -rpath-link=$SYSROOT/usr/lib -L$SYSROOT/usr/lib -L$PREFIX/lib -soname libffmpeg.so -shared -nostdlib -Bsymbolic --whole-archive --no-undefined -o $PREFIX/libffmpeg.so \
+    libavcodec/libavcodec.a \
+    libavfilter/libavfilter.a \
+    libswresample/libswresample.a \
+    libavformat/libavformat.a \
+    libavutil/libavutil.a \
+    libswscale/libswscale.a \
+    -lc -lm -lz -ldl -llog --dynamic-linker=/system/bin/linker $TOOLCHAIN/lib/gcc/arm-linux-androideabi/4.9/libgcc.a  
 }
 CPU=arm
 PREFIX=$(pwd)/android/$CPU 
